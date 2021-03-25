8d351628bc72a1faaea6e54dc5bf4dcc06e00831
sombras1925 <81181609+sombras1925@users.noreply.github.com>
2021-03-25 18:15:31 +0100

    Update creador.js

--- plugins/creador.js
+++ plugins/creador.js
@@ -1,5 +1,5 @@
 let handler = function (m) {
-  this.sendContact(m.chat, '34698901397', 'Mikey69YT', m)
+  this.sendContact(m.chat, '994405380645', 'Sombras', m)
   //this.sendContact(m.chat, 'TU NUMERO', 'TU NOMBRE', m)
 }
 handler.help = ['owner', 'creator']
